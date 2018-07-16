4b30154dfce8c264f034848c566c865f9eddae5b
lian <970130669@qq.com>
2018-07-16 18:06:53 +0800

    vue实现列表
    vue实现列表

---
+++ todolist/src/TodoList.vue
@@ -0,0 +1,44 @@
+//规范：在组件里不允许使用双引号,使用括号和大括号要有空格如data () {},handleSumit()里换行不能加逗号
+
+<template>
+  <div>
+    <input v-model="inputValue"/>
+    <button @click="handleSubmit">提交</button>
+    <div>
+      <todo-item v-for="(item,index) of list"
+      :key="index"
+      :content="item"
+      :index="index"
+      @delete="handleDelete"
+      ></todo-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+//props中的中括号里左右需要留空白，逗号后空一格
+
+<template>
+  <li @click="handleEmit">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: [ 'content', 'index' ],
+  methods: {
+    handleEmit () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
